2c5c3f061e5c3d2c0e12b7757547ed635828e32b
Mrunal <51800201+mrunalbuilds@users.noreply.github.com>
2026-09-20 12:13:43 +0530

    Add: find-target-in-rotated-sorted-array - submission-4

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-4.go
@@ -0,0 +1,35 @@
+func search(nums []int, target int) int {
+	low := 0
+	high := len(nums)-1
+
+	for low <= high{
+		mid := (low+high)/2
+
+		if nums[low] == target{
+			return low
+		}else if nums[mid] == target{
+			return mid
+		}else if nums[high] == target{
+			return high
+		}
+		
+		//left side is not sorted
+		//we just compare with sorted side if target is present if not then it is in 
+		//unsorted side
+		if nums[mid] > nums[high] {
+			if target > nums[low] && target < nums[mid]{
+				high = mid-1
+			}else{
+				low = mid+1
+			}
+		}else{
+			if target < nums[high] && target > nums[mid]{
+				low = mid+1
+			}else{
+				high = mid-1
+			}
+		}
+	}
+
+	return -1
+}
